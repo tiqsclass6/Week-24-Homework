bbc55cafe2bed70cb873641487e2926260d78fe7
tiqsclass6 <daquietstorm22@gmail.com>
2025-07-13 19:49:58 -0700

    fifth commit

--- 15-Backend.tf
+++ 15-Backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
-    bucket  = "your-bucket-name"     # Name of the S3 bucket
-    key     = "dateortime.tfstate" # The name of the state file in the bucket
+    bucket  = "tiqs-tf-state-files"     # Name of the S3 bucket
+    key     = "week-24-homework.tfstate" # The name of the state file in the bucket
     region  = "us-east-1"               # Choose your region
     encrypt = true                      # Enable server-side encryption (optional but recommended)
   }
